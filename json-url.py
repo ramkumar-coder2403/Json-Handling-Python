1ec23152ecc5d02407bd456ffc2c559f2e96a70c
Ramkumar Megavarnan <67353220+ramkumar-coder2403@users.noreply.github.com>
2022-03-04 10:20:06 +0530

    Update json-url.py

--- json-url.py
+++ json-url.py
@@ -1,21 +1,16 @@
 import requests
-import sys
-
 
 r = requests.get("https://api.npoint.io/53141e20efcc35709569")
 data = r.json()
 
-# data = json.load(f)
+tot = 0
 
 print('\n')
 print("Serial number", "\t", "Student name", "\t", "Student number")
 print("==============", "\t", "============", "\t", "==============")
-idx = 0
-sl = 0
-tot = 0
+
 for i in range(len(data['Students'])):
-    idx += 1
-    print(idx, "\t\t", data['Students'][i]['Name'],
+    print(i+1, "\t\t", data['Students'][i]['Name'],
           "\t\t", data['Students'][i]['Number'])
 
 while True:
@@ -23,14 +18,15 @@ while True:
         "Enter the Serial Number for more details or type exit to quit: ")
     try:
         n_val = int(val)
-        if n_val == 0 or n_val > idx:
+        if n_val == 0 or n_val > len(data['Students']):
             None
         else:
             print('\n')
             print("Name of the Student : ",
-                data['Students'][n_val-1]['Name'], '\n')
+                  data['Students'][n_val-1]['Name'], '\n')
 
-            print("Serial number", "\t", "Subject", "\t", "Mark", "\t", "Status")
+            print("Serial number", "\t", "Subject",
+                  "\t", "Mark", "\t", "Status")
             print("==============================================")
 
             for j in range(len(data['Marks'])):
@@ -38,21 +34,19 @@ while True:
                     for key in data['Marks'][j]:
                         if key == 'Number':
                             continue
-                        sl += 1
                         tot += int(data['Marks'][j][key])
                         if int(data['Marks'][j][key]) < 35:
                             sts = 'fail'
                         else:
                             sts = 'pass'
 
-                        print(sl, "\t\t", key, "\t",
-                            data['Marks'][j][key], "\t", sts)
+                        print(list(data['Marks'][j]).index(key), "\t\t", key, "\t",
+                              data['Marks'][j][key], "\t", sts)
 
             print("==============================================")
             print('Total', "\t\t\t\t", tot)
-            tot=0
-            sl=0
             print("==============================================")
+            tot = 0
     except ValueError:
         if val == 'exit':
             # sys.exit()
